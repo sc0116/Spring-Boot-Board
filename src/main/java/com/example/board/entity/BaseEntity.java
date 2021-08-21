a5bb750685cd2be3a2aa5d3d139fbfba8bada067
SeungCheol <ssc6839@gmail.com>
2021-08-21 21:52:49 +0900

    연관관계 테스트

---
+++ src/main/java/com/example/board/entity/BaseEntity.java
@@ -0,0 +1,23 @@
+package com.example.board.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(value = {AuditingEntityListener.class})
+@Getter
+abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "regdate", updatable = false)
+    private LocalDateTime regDate;
+
+    @LastModifiedDate
+    @Column(name = "moddate")
+    private LocalDateTime modDate;
+}
