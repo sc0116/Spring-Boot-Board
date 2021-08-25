8308fce246b452446e93b755f9cc06afda953729
SeungCheol <ssc6839@gmail.com>
2021-08-25 18:38:43 +0900

    댓글 삭제 처리 오류 수정

--- src/main/java/com/example/board/repository/ReplyRepository.java
+++ src/main/java/com/example/board/repository/ReplyRepository.java
@@ -4,10 +4,11 @@ import com.example.board.entity.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
 
     @Modifying
     @Query("delete from Reply r where r.board.bno = :bno")
-    void deleteByBno(Long bno);
+    void deleteByBno(@Param("bno") Long bno);
 }
